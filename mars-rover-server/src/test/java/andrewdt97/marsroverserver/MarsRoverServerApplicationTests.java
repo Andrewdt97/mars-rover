93defb620f62cdccef437184add117214219913a
andrew <andrewdt97@gmail.com>
2020-06-02 14:55:48 -0400

    Springboot init commit

---
+++ mars-rover-server/src/test/java/andrewdt97/marsroverserver/MarsRoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package andrewdt97.marsroverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
